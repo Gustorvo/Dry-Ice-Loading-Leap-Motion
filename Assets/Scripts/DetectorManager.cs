507c8fdcd9e7fb5fbe90e00fb22d7929f5b4e6b4
Gustorvo <mumiasms@gmail.com>
2019-04-28 08:20:34 +0200

    implemented Hand Models Toggle functionality

---
+++ Assets/Scripts/DetectorManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+public class DetectorManager : MonoBehaviour
+{
+    public HandModelBase[] CapsulHands;
+    public HandModelBase[] RiggedHands;
+
+    public GameObject PalmDetector;
+    public GameObject PinchDetector;
+
+    private int current = 1; // first element
+
+    // Use this for initialization
+
+
+   public void ToggleDetectors()
+    {
+        if (current == 2) // reached the last lement, toggle back
+        {
+            DoJob(CapsulHands[0]);// first element of Hands Array is a left hand
+            current = 1;
+        }
+        else
+        {
+            DoJob(RiggedHands[0]);
+            current = 2;
+        }
+    }
+
+    void DoJob(HandModelBase model)
+    {
+        PalmDetector.GetComponent<PalmDirectionDetector>().HandModel = model;
+        PinchDetector.GetComponent<PinchDetector>().HandModel = model;
+    }
+}
---
+++ Assets/Scripts/ToggleHandsModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity;
+
+public class ToggleHandsModel : MonoBehaviour {
+
+    public HandModelManager HandPool;
+    public string[] HandGroups;
+    [SerializeField]
+    private int current;
+    public DetectorManager _detectorManagerScript;
+   
+    
+
+
+    void Start () {
+        HandPool = GetComponent<HandModelManager>(); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        ToggleHands();
+
+    }
+
+    public void ToggleHands()
+    {
+        HandPool.DisableGroup(HandGroups[current]);
+        if (current == HandGroups.Length - 1) // reached the last element
+            current = 0; // start with the first element
+        else current++;
+        HandPool.EnableGroup(HandGroups[current]);
+
+        _detectorManagerScript.ToggleDetectors(); // toggle detectors for new hands
+
+
+    }
+
+    
+
+    
+}
